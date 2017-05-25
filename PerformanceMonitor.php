a06ba0ccf63f3d761db8c8d52f63728eab650d7f
Andrej Kabachnik <andrej_ka@gmx.de>
2017-05-25 10:33:08 +0200

    REFACTOR code style now compatible with PSR-12

--- PerformanceMonitor.php
+++ PerformanceMonitor.php
@@ -1,4 +1,5 @@
-<?php namespace exface\PerformanceMonitor;
+<?php
+namespace exface\PerformanceMonitor;
 
 use Symfony\Component\Stopwatch\Stopwatch;
 use exface\Core\CommonLogic\UxonObject;
@@ -6,72 +7,85 @@ use exface\Core\Events\ActionEvent;
 use exface\Core\Events\DataConnectionEvent;
 use exface\Core\Interfaces\Actions\ActionInterface;
 
-class PerformanceMonitor {
-	private $stopwatch = null;
-	private $offset = 0;
-	
-	public function __construct(){
-		$this->stopwatch = new Stopwatch();
-		$this->start();
-	}
-	
-	public function start_action(ActionEvent $event){
-		$this->stopwatch->start($event->get_action()->get_id());
-	}
-	
-	public function stop_action(ActionEvent $event){
-		try {
-			$this->stopwatch->stop($event->get_action()->get_id());
-		} catch (\Exception $e){
-			// Do nothing if stopping goes wrong...
-		}
-	}
-	
-	public function start_data_source_query(DataConnectionEvent $event){
-		$category = new UxonObject();
-		$category->set_property('query', $event->get_current_query());
-		$this->stopwatch->start($event->get_current_query(), $category->to_json());
-	}
-	
-	public function stop_data_source_query(DataConnectionEvent $event){
-		$this->stopwatch->stop($event->get_current_query());
-	}
-	
-	public function export_uxon_object(){
-		$uxon = new UxonObject();
-		$uxon->set_property('TOTAL', $this->stopwatch->getEvent('TOTAL')->getDuration());
-		return $uxon;
-	}
-		
-	public function start(){
-		$this->stopwatch->start('TOTAL');
-	}
-	
-	public function stop(){
-		$this->stopwatch->stop('TOTAL');
-	}
-	
-	public function get_offset() {
-		return $this->offset;
-	}
-	
-	/**
-	 * 
-	 * @param integer $milliseconds
-	 */
-	public function set_offset($milliseconds) {
-		$this->offset = $milliseconds;
-		return $this;
-	}
-	
-	public function get_action_duration(ActionInterface $action){
-		try {
-			$result = $this->stopwatch->getEvent($action->get_id())->getDuration();
-		} catch (\Exception $e){
-			$result = NULL;
-		}
-		return $result;
-	}
-  
+class PerformanceMonitor
+{
+
+    private $stopwatch = null;
+
+    private $offset = 0;
+
+    public function __construct()
+    {
+        $this->stopwatch = new Stopwatch();
+        $this->start();
+    }
+
+    public function startAction(ActionEvent $event)
+    {
+        $this->stopwatch->start($event->getAction()->getId());
+    }
+
+    public function stopAction(ActionEvent $event)
+    {
+        try {
+            $this->stopwatch->stop($event->getAction()->getId());
+        } catch (\Exception $e) {
+            // Do nothing if stopping goes wrong...
+        }
+    }
+
+    public function startDataSourceQuery(DataConnectionEvent $event)
+    {
+        $category = new UxonObject();
+        $category->setProperty('query', $event->getCurrentQuery());
+        $this->stopwatch->start($event->getCurrentQuery(), $category->toJson());
+    }
+
+    public function stopDataSourceQuery(DataConnectionEvent $event)
+    {
+        $this->stopwatch->stop($event->getCurrentQuery());
+    }
+
+    public function exportUxonObject()
+    {
+        $uxon = new UxonObject();
+        $uxon->setProperty('TOTAL', $this->stopwatch->getEvent('TOTAL')->getDuration());
+        return $uxon;
+    }
+
+    public function start()
+    {
+        $this->stopwatch->start('TOTAL');
+    }
+
+    public function stop()
+    {
+        $this->stopwatch->stop('TOTAL');
+    }
+
+    public function getOffset()
+    {
+        return $this->offset;
+    }
+
+    /**
+     *
+     * @param integer $milliseconds            
+     */
+    public function setOffset($milliseconds)
+    {
+        $this->offset = $milliseconds;
+        return $this;
+    }
+
+    public function getActionDuration(ActionInterface $action)
+    {
+        try {
+            $result = $this->stopwatch->getEvent($action->getId())->getDuration();
+        } catch (\Exception $e) {
+            $result = NULL;
+        }
+        return $result;
+    }
 }
 ?>
--- PerformanceMonitorApp.php
+++ PerformanceMonitorApp.php
@@ -1,32 +1,44 @@
-<?php namespace exface\PerformanceMonitor;
+<?php
+namespace exface\PerformanceMonitor;
 
 use exface\Core\CommonLogic\AbstractApp;
 
-class PerformanceMonitorApp extends AbstractApp {
-	private $monitor = null;
-	
-	protected function init(){
-		$this->monitor = new PerformanceMonitor();
-		$this->register_listeners();
-	}
-	
-	/**
-	 * @return PerformanceMonitor
-	 */
-	public function get_monitor(){
-		return $this->monitor;
-	}
-	
-	/**
-	 * 
-	 * @return PerformanceMonitorApp
-	 */
-	public function register_listeners(){
-		$event_manager = $this->get_workbench()->event_manager();
-		$event_manager->add_listener('#.Action.Perform.Before', array($this->monitor, 'start_action'));
-		$event_manager->add_listener('#.Action.Perform.After', array($this->monitor, 'stop_action'));
-		return $this;
-	}
-	
+class PerformanceMonitorApp extends AbstractApp
+{
+
+    private $monitor = null;
+
+    protected function init()
+    {
+        $this->monitor = new PerformanceMonitor();
+        $this->registerListeners();
+    }
+
+    /**
+     *
+     * @return PerformanceMonitor
+     */
+    public function getMonitor()
+    {
+        return $this->monitor;
+    }
+
+    /**
+     *
+     * @return PerformanceMonitorApp
+     */
+    public function registerListeners()
+    {
+        $event_manager = $this->getWorkbench()->eventManager();
+        $event_manager->addListener('#.Action.Perform.Before', array(
+            $this->monitor,
+            'startAction'
+        ));
+        $event_manager->addListener('#.Action.Perform.After', array(
+            $this->monitor,
+            'stopAction'
+        ));
+        return $this;
+    }
 }
 ?>
